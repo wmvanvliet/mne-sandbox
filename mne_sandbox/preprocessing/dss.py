34cf30b90044daddd021b73ce2180c9bbe06c61c
drammock <dan.mccloy@gmail.com>
2016-04-21 16:17:31 -0700

    more efficient linalg; handle both ndarray and Epochs; better docs

--- mne_sandbox/preprocessing/dss.py
+++ mne_sandbox/preprocessing/dss.py
@@ -1,90 +1,119 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 19 12:47:10 2016
-@author: drmccloy
-"""
+
+"""Denoising source separation"""
+
+# Authors: Daniel McCloy <drmccloy@uw.edu>
+#
+# License: BSD (3-clause)
 
 import numpy as np
+from mne import Epochs, compute_covariance
 
 
-def dss(epochs, data_n_keep=None, bias_n_keep=None, data_thresh=None,
-        bias_thresh=None):
-    """Preprocess physiological data with denoising source separation (DSS)
+def dss(data, data_max_components=None, data_thresh=0,
+        bias_max_components=None, bias_thresh=0, return_data=True):
+    """Process physiological data with denoising source separation (DSS)
 
-    Implementation follows the procedure described in S\"arel\"a & Valpola [1]
-    and de Cheveign\'e & Simon [2].
+    Implementation follows the procedure described in Särelä & Valpola [1]_
+    and de Cheveigné & Simon [2]_.
 
     Parameters
     ----------
-    data : instance of Epochs
+    data : instance of Epochs | array of shape (n_trials, n_channels, n_times)
         Data to be denoised.
-    data_n_keep, bias_n_keep : int
-        Number of components to keep during PCA decomposition of the data and
-        bias function. ``None`` (the default) means keep all suprathreshold
-        components (see ``data_thresh``).
-   data_thresh, bias_thresh : float
-       Threshold (relative to the largest component) below which components
-       will be discarded during decomposition of the data and bias function.
-       For example, if the largest eigenvalue is 3 and ``thresh=0.1``,
-       components with eigenvalues below 0.3 will be discarded.
+    data_max_components : int | None
+        Maximum number of components to keep during PCA decomposition of the
+        data. ``None`` (the default) keeps all suprathreshold components.
+    data_thresh : float | None
+        Threshold (relative to the largest component) above which components
+        will be kept during decomposition of the data. The default keeps all
+        non-zero values; to keep all values, specify ``thresh=None``.
+    bias_max_components : int | None
+        Maximum number of components to keep during PCA decomposition of the
+        bias function. ``None`` (the default) keeps all suprathreshold
+        components.
+    bias_thresh : float | None
+        Threshold (relative to the largest component) below which components
+        will be discarded during decomposition of the bias function. ``None``
+        (the default) keeps all non-zero values; to keep all values, pass
+        ``thresh=None`` and ``max_components=None``.
+    return_data : bool
+        Whether to return the denoised data along with the denoising matrix.
+
+    Returns
+    -------
+    dss_mat : array of shape (n_channels, n_dss_channels)
+        The denoising matrix.
+    dss_data : array of shape (n_trials, n_dss_channels, n_times)
+        The denoised data. Note that n_dss_channels are orthogonal virtual
+        channels and may be fewer in number than the number of channels in the
+        input Epochs object. Returned only if ``return_data`` is ``True``.
 
     References
     ----------
-    [1] S\"arel\"a, Jaakko, and Valpola, Harri (2005). Denoising source
-    separation.  Journal of Machine Learning Research 6: 233–72.
+    .. [1] Särelä, Jaakko, and Valpola, Harri (2005). Denoising source
+    separation. Journal of Machine Learning Research 6: 233–72.
 
-    [2] de Cheveign\'e, Alain, and Simon, Jonathan Z. (2008). Denoising based
+    .. [2] de Cheveigné, Alain, and Simon, Jonathan Z. (2008). Denoising based
     on spatial filtering. Journal of Neuroscience Methods, 171(2): 331-339.
     """
-    # TODO: allow Raw as well as Epochs?
-    # TODO: implement mean-subtraction? optional?
-    # TODO: apply picks before DSS?
-    evoked = epochs.average()
-    bias_cov = np.dot(evoked, evoked.T)
-    data_cov = np.sum([np.dot(trial, trial.T) for trial in epochs], axis=0)
-    dss_mat = _dss(data_cov, bias_cov, data_n_keep, bias_n_keep, data_thresh,
-                   bias_thresh)
-    data = epochs.get_data()
-    n_trials, n_chans, n_times = data.shape
-    data = data.transpose((1, 2, 0)).reshape(n_chans, n_times * n_trials)
-    epochs._data = np.dot(dss_mat.T, data).reshape(dss_mat.shape[1], n_times,
-                                                   n_trials).transpose(2, 0, 1)
-    return epochs
+    if isinstance(data, Epochs):
+        data_cov = compute_covariance(data).data
+        bias_cov = np.cov(data.average().data)
+        if return_data:
+            data = data.get_data()
+    elif isinstance(data, np.ndarray):
+        data_cov = np.sum([np.dot(trial, trial.T) for trial in data], axis=0)
+        bias_cov = np.cov(data.mean(axis=0))
+    else:
+        raise TypeError('Data to denoise must be an instance of mne.Epochs or '
+                        'a numpy array.')
+    dss_mat = _dss(data_cov, bias_cov, data_max_components, data_thresh,
+                   bias_max_components, bias_thresh)
+    if return_data:
+        n_trials, n_chans, n_times = data.shape
+        dss_data = np.einsum('ij,hik->hjk', dss_mat, data)
+        return dss_mat, dss_data
+    else:
+        return dss_mat
 
 
-def _dss(data_cov, bias_cov, data_n_keep=None, bias_n_keep=None,
-         data_thresh=None, bias_thresh=None):
-    """Preprocess physiological data with denoising source separation (DSS)
+def _dss(data_cov, bias_cov, data_max_components=None, data_thresh=None,
+         bias_max_components=None, bias_thresh=None):
+    """Process physiological data with denoising source separation (DSS)
 
     Acts on covariance matrices; allows specification of arbitrary bias
     functions (as compared to the public ``dss`` function, which forces the
     bias function to be the evoked response).
     """
-    data_eigval, data_eigvec = _pca(data_cov, data_n_keep, data_thresh)
-    W = np.diag(np.sqrt(1 / data_eigval))  # whitening matrix
+    data_eigval, data_eigvec = _pca(data_cov, data_max_components, data_thresh)
+    W = np.sqrt(1 / data_eigval)  # diagonal of whitening matrix
     # bias covariance projected into whitened PCA space of data channels
-    bias_white = W.T.dot(data_eigvec.T).dot(bias_cov).dot(data_eigvec).dot(W)
+    bias_cov_white = (W * data_eigvec).T.dot(bias_cov).dot(data_eigvec) * W
     # proj. matrix from whitened data space to a space maximizing bias fxn
-    bias_eigval, bias_eigvec = _pca(bias_white, bias_n_keep, bias_thresh)
+    bias_eigval, bias_eigvec = _pca(bias_cov_white, bias_max_components,
+                                    bias_thresh)
     # proj. matrix from data to bias-maximizing space (DSS space)
-    dss_mat = data_eigvec.dot(W).dot(bias_eigvec)
-    # matrix to normalize DSS dimensions
-    N = np.diag(np.sqrt(1 / np.diag(dss_mat.T.dot(data_cov).dot(dss_mat))))
-    return dss_mat.dot(N)
+    dss_mat = (W[np.newaxis, :] * data_eigvec).dot(bias_eigvec)
+    # normalize DSS dimensions
+    N = np.sqrt(1 / np.diag(dss_mat.T.dot(data_cov).dot(dss_mat)))
+    return N[np.newaxis, :] * dss_mat
 
 
-def _pca(cov, n_keep=None, thresh=None):
+def _pca(cov, max_components=None, thresh=0):
     """Perform PCA decomposition
 
     Parameters
     ----------
     cov : array-like
         Covariance matrix
-    n_keep : int | None
-        Number of components to retain after decomposition.
-    thresh : float
-        Threshold (relative to the largest component) below which components
-        will be discarded.
+    max_components : int | None
+        Maximum number of components to retain after decomposition. ``None``
+        (the default) keeps all suprathreshold components (see ``thresh``).
+    thresh : float | None
+        Threshold (relative to the largest component) above which components
+        will be kept. The default keeps all non-zero values; to keep all
+        values, specify ``thresh=None`` and ``max_components=None``.
 
     Returns
     -------
@@ -93,16 +122,19 @@ def _pca(cov, n_keep=None, thresh=None):
     eigvec : array
         2-dimensional array of eigenvectors.
     """
-    eigval, eigvec = np.linalg.eig(cov)
+
+    if thresh > 1 or thresh < 0:
+        raise ValueError('Threshold must be between 0 and 1 (or None).')
+    eigval, eigvec = np.linalg.eigh(cov)
     eigval = np.abs(eigval)
     sort_ix = np.argsort(eigval)[::-1]
     eigvec = eigvec[:, sort_ix]
     eigval = eigval[sort_ix]
-    if n_keep is not None:
-        eigval = eigval[:n_keep]
-        eigvec = eigvec[:, :n_keep]
+    if max_components is not None:
+        eigval = eigval[:max_components]
+        eigvec = eigvec[:, :max_components]
     if thresh is not None:
-        suprathresh = np.where(eigval / eigval.max() >= thresh)[0]
+        suprathresh = np.where(eigval / eigval.max() > thresh)[0]
         eigval = eigval[suprathresh]
         eigvec = eigvec[:, suprathresh]
     return eigval, eigvec
